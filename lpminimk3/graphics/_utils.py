a770f3d3e4fb0d8146c851c04cd2bb61cb7e7525
Chronic Coder <efeoghene.obebeduo@gmail.com>
2021-04-12 18:37:56 -0700

    Add feature to change bit representation in console

--- lpminimk3/graphics/_utils.py
+++ lpminimk3/graphics/_utils.py
@@ -557,6 +557,7 @@ class String(Renderable):
                                              self._glyph_dicts,
                                              fg_color,
                                              bg_color)
+        self._text = text
         self._characters = list(characters)
         self._angle = 0
         self._text_scroll = None
@@ -626,7 +627,7 @@ class String(Renderable):
                                     one=one,
                                     zero=zero)
         else:
-            print(self.character_to_render.raw_bitmap)
+            self._print_in_console(one=one, zero=zero)
 
     def shift_left(self, *, count=1, circular=True):
         count = 0 if not isinstance(count, int) or count < 0 else count
@@ -689,3 +690,13 @@ class String(Renderable):
                 glyph_dicts.append(GlyphDictionary(os.path.join(glyph_dict_dir, filename),  # noqa
                                    './schema/glyph.schema.json'))
         return glyph_dicts
+
+    def _print_in_console(self, one='X', zero=' '):
+        for index, bit in enumerate(self.character_to_render.raw_bitmap,  # noqa
+                                    start=1):
+            if bit:
+                print(one, end='')
+            else:
+                print(zero, end='')
+            if index % self.character_to_render.word_count == 0:
+                print('\n', end='')
