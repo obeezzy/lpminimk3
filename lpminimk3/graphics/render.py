c8526e76c18b184b65fda5cde73bf6de9f7e5c9f
Chronic Coder <efeoghene.obebeduo@gmail.com>
2021-04-13 09:12:29 -0700

    Update render script

--- lpminimk3/graphics/render.py
+++ lpminimk3/graphics/render.py
@@ -7,27 +7,13 @@ def print_usage():
     print(f'Usage: {os.path.basename(sys.argv[0])} CHARACTER\n')  # noqa
 
 
-def render(text):
-    for index, bit in enumerate(text.bits, start=1):
-        if bit:
-            print('X', end='')
-        elif 'LOGLEVEL' in os.environ:
-            print('.', end='')
-        else:
-            print(' ', end='')
-
-        if index % text.word_count == 0:
-            print('\n', end='')
-
-
 def main():
     try:
         if len(sys.argv) != 2:
             raise ValueError('Missing character argument.')
 
         input_string = sys.argv[1]
-        text = Text(input_string[0])
-        render(text)
+        Text(input_string[0]).print()
     except Exception as e:
         print(e, file=sys.stderr)
         print_usage()
