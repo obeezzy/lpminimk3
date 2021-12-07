ed0846a377e6c2e9a81edd0c2c9b2658795c49f2
Chronic Coder <efeoghene.obebeduo@gmail.com>
2021-12-07 04:08:04 -0800

    Ignore unused import in lpminimk3/__init__.py

--- lpminimk3/__init__.py
+++ lpminimk3/__init__.py
@@ -1,4 +1,4 @@
 from .__version__ import __version__, VERSION  # noqa
 from ._core.components import Grid, Panel, Led, ButtonFace  # noqa
 from ._core.utils import Interface, Mode, Layout, ButtonEvent  # noqa
-from ._lpminimk3 import LaunchpadMiniMk3, find_launchpads
+from ._lpminimk3 import LaunchpadMiniMk3, find_launchpads  # noqa
