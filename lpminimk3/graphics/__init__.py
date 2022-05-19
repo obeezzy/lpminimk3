4d23458b91eaf0e62215c47bcddafbe236366727
Chronic Coder <efeoghene.obebeduo@gmail.com>
2022-05-18 17:38:46 -0700

    Make Frame public

--- lpminimk3/graphics/__init__.py
+++ lpminimk3/graphics/__init__.py
@@ -1,6 +1,7 @@
 from ._graphics import Text,\
                        Bitmap,\
                        Movie,\
+                       Frame,\
                        Renderable,\
                        ScrollDirection,\
                        FlipAxis  # noqa
