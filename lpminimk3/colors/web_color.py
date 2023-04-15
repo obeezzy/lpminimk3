1fdeaef91b5695dd616b6abb28adfb0ec8b44dda
Chronic Coder <efeoghene.obebeduo@gmail.com>
2023-04-15 05:49:08 -0700

    Add WebColor documentation

--- lpminimk3/colors/web_color.py
+++ lpminimk3/colors/web_color.py
@@ -1,12 +1,40 @@
+"""Web color.
+
+This module is an abstraction over web colors. The full list
+of colors can be found on
+`Wikipedia <https://en.wikipedia.org/wiki/Lists_of_colors>`_.
+
+Example
+-------
+Set color of LED "0x0" to "amethyst":
+>>> import lpminimk3
+>>> lp = lpminimk3.find_launchpads()[0]
+>>> lp.open()
+>>> lp.mode = 'prog'
+>>> lp.grid.led('0x0').color = lpminimk3.colors.WebColor("amethyst")
+"""
+
 import os
 import json
 import jsonschema
 
 
 class WebColorDictionary:
+    """Web color dictionary.
+    """
     def __init__(self,
                  json_filename, *,
                  schema_filename='./web_colors.schema.json'):
+        """Constructs a web color dictionary from JSON file
+        `json_filename`.
+
+        Parameters
+        ----------
+        json_filename : str
+            Name of JSON file to parse.
+        schema_filename : str, optional
+            Name of schema JSON file.
+        """
         self._filename = self._determine_abspath(json_filename)
         self._data = self._load(json_filename)
         schema = self._load(schema_filename)
@@ -29,10 +57,14 @@ class WebColorDictionary:
 
     @property
     def filename(self):
+        """File name.
+        """
         return self._filename
 
     @property
     def data(self):
+        """Data.
+        """
         return self._data
 
     def _load(self, filename):
@@ -56,23 +88,56 @@ class WebColorDictionary:
 
 
 class WebColor:
+    """Web color.
+
+    Example
+    -------
+    Set color of LED "0x0" to "amethyst":
+    >>> import lpminimk3
+    >>> from lpminimk3.colors import WebColor
+    >>> lp = lpminimk3.find_launchpads()[0]
+    >>> lp.open()
+    >>> lp.mode = 'prog'
+    >>> lp.grid.led('0x0').color = WebColor("amethyst")
+    """
+
+    """Web color dictionary.
+    """
+    COLOR_DICT = WebColorDictionary('./web_colors.json')
+
     def __init__(self, name):
-        self._web_dictionary = WebColorDictionary('./web_colors.json')
+        """Constructs a web color with name `name`.
+
+        Parameters
+        ----------
+        name : str
+            Name of web color.
+        """
         self._name = name
 
     @property
     def name(self):
+        """Name.
+        """
         return self._name
 
     @property
     def rgb(self):
-        return self._web_dictionary[self._name]['rgb']
+        """RGB value.
+        Its possible values range from 0 to 255.
+        """
+        return WebColor.COLOR_DICT[self._name]['rgb']
 
     @property
     def rgb_normalized(self):
-        r, g, b = self._web_dictionary[self._name]['rgb']
+        """RGB value, normalized to work on a Launchpad.
+        Its possible values range from 0 to 127.
+        """
+        r, g, b = self.COLOR_DICT[self._name]['rgb']
         return (r >> 1, g >> 1, b >> 1)
 
     @property
     def hex(self):
-        return self._web_dictionary[self._name]['hex']
+        """Hex color.
+        """
+        return self.WEB_COLOR_DICTIONARY[self._name]['hex']
