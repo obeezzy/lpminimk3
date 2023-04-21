03e76ed129d28950cb0ddb6e1fdcbe87c2288855
Chronic Coder <efeoghene.obebeduo@gmail.com>
2023-04-21 08:56:53 -0700

    Add logos.py example
    Add more bitmaps

--- lpminimk3/graphics/art.py
+++ lpminimk3/graphics/art.py
@@ -4,7 +4,16 @@ _CURRENT_DIR = os.path.dirname(__file__)
 
 
 class Bitmaps:
+    APPLE = os.path.join(_CURRENT_DIR, 'bitmaps/apple.bitmap.json')  # noqa
+    APPLE_OLD = os.path.join(_CURRENT_DIR, 'bitmaps/apple_old.bitmap.json')  # noqa
+    FACEBOOK = os.path.join(_CURRENT_DIR, 'bitmaps/facebook.bitmap.json')  # noqa
+    GMAIL = os.path.join(_CURRENT_DIR, 'bitmaps/gmail.bitmap.json')  # noqa
+    GOOGLE = os.path.join(_CURRENT_DIR, 'bitmaps/google.bitmap.json')  # noqa
+    ITUNES = os.path.join(_CURRENT_DIR, 'bitmaps/itunes.bitmap.json')  # noqa
     SOCKET = os.path.join(_CURRENT_DIR, 'bitmaps/socket.bitmap.json')  # noqa
+    WINDOWS = os.path.join(_CURRENT_DIR, 'bitmaps/windows.bitmap.json')  # noqa
+    XBOX = os.path.join(_CURRENT_DIR, 'bitmaps/xbox.bitmap.json')  # noqa
+    YOUTUBE = os.path.join(_CURRENT_DIR, 'bitmaps/youtube.bitmap.json')  # noqa
 
 
 class Movies:
