d3151b87f7ed939c149c851520c3884401e4e19d
Chronic Coder <efeoghene.obebeduo@gmail.com>
2023-04-27 10:26:09 -0700

    Change 'socket' bitmap to 'plug' bitmap

--- lpminimk3/graphics/art.py
+++ lpminimk3/graphics/art.py
@@ -10,7 +10,7 @@ class Bitmaps:
     GMAIL = os.path.join(_CURRENT_DIR, 'bitmaps/gmail.bitmap.json')  # noqa
     GOOGLE = os.path.join(_CURRENT_DIR, 'bitmaps/google.bitmap.json')  # noqa
     ITUNES = os.path.join(_CURRENT_DIR, 'bitmaps/itunes.bitmap.json')  # noqa
-    SOCKET = os.path.join(_CURRENT_DIR, 'bitmaps/socket.bitmap.json')  # noqa
+    PLUG = os.path.join(_CURRENT_DIR, 'bitmaps/plug.bitmap.json')  # noqa
     WINDOWS = os.path.join(_CURRENT_DIR, 'bitmaps/windows.bitmap.json')  # noqa
     XBOX = os.path.join(_CURRENT_DIR, 'bitmaps/xbox.bitmap.json')  # noqa
     YOUTUBE = os.path.join(_CURRENT_DIR, 'bitmaps/youtube.bitmap.json')  # noqa
